801c7105a90547bec9eaab514bd75470bb08f0d1
odieatla <ozhang@undertone.com>
2013-03-26 21:17:13 -0700

    revert int.

---
+++ RevertInt.java
@@ -0,0 +1,28 @@
+public class Solution {
+    public int revert(int x) {
+        if(x == 0)
+        {
+            return 0;
+        }
+
+        int flag = 1;
+
+        if(x < 0)
+        {
+            flag = -1;
+        }
+
+        int out = 0;
+
+        int y = x * flag;
+
+        do {
+            out = out * 10 + y%10;
+
+            y = y/10;
+        }
+        while(y > 0);
+
+        return out * flag;
+    }
+}
