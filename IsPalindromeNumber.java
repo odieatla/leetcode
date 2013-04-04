a7e693de6e2d9efe4e6cfd77741b10c8b12eedaa
odieatla <ozhang@undertone.com>
2013-04-03 21:07:14 -0700

    palindrome number.

---
+++ IsPalindromeNumber.java
@@ -0,0 +1,33 @@
+public class Solution {
+    public boolean isPalindrome(int x) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        if(x < 0)
+        {
+            return false;
+        }
+
+        int div = 1;
+
+        while(x/div >= 10) //watch out 1001, 101
+        {
+            div *= 10;
+        }
+
+        while(x > 0)
+        {
+            int last = x%10;
+            int first = x/div;
+
+            if(first - last != 0)
+            {
+                return false;
+            }
+
+            x = (x%div)/10;
+            div /= 100;
+        }
+
+        return true;
+    }
+}
