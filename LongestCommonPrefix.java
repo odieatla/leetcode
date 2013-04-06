4fdb65b0dc23bc0955e0759064cc48e4ab526d38
odieatla <ozhang@undertone.com>
2013-04-06 15:21:54 -0700

    longest common prefix

---
+++ LongestCommonPrefix.java
@@ -0,0 +1,32 @@
+public class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        if(strs.length == 0)
+        {
+            return "";
+        }
+        if(strs.length == 1)
+        {
+          return strs[0];
+        }
+        
+        for(int i = 0; i < strs[0].length(); i++)
+        {
+          for(int j = 1; j < strs.length; j++)
+          {
+            if(i == strs[j].length())
+            {
+              return strs[0].substring(0, i);
+            }
+            
+            if(strs[j].charAt(i) != strs[0].charAt(i))
+            {
+              return strs[0].substring(0, i);
+            }
+          }
+        }
+        
+      return strs[0];
+    }
+}
