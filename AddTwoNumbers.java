3a0a03ccfa0bd2a5397740771f15f880a990aa3f
Ouning Zhang <ozhang@undertone.com>
2013-05-27 01:45:48 -0700

    Add Two Numbers -- more neat code.

--- AddTwoNumbers.java
+++ AddTwoNumbers.java
@@ -67,4 +67,27 @@ public class Solution {
 
         return head.next;
     }
+
+    //more neat code
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode l3 = new ListNode(0);
+        ListNode head = l3;
+        int carry = 0;
+
+        while(l1 != null || l2 != null || carry > 0)
+        {
+            int n1 = (l1 == null) ? 0 : l1.val;
+            int n2 = (l2 == null) ? 0 : l2.val;
+            int v = n1 + n2 + carry;
+            carry = v/10;
+
+            l3.next = new ListNode(v%10);
+
+            l1 = (l1 == null) ? null : l1.next;
+            l2 = (l2 == null) ? null : l2.next;
+            l3 = l3.next;
+        }
+
+        return head.next;
+    }
 }
